9a289abce4ba9635f07899f47e6e7b3e97512910
jlvihv <vihv@qq.com>
2023-01-11 18:11:39 +0800

    add starknet code examples

--- src/model/code_examples/starkware.rs
+++ src/model/code_examples/starkware.rs
@@ -1,9 +1,48 @@
 use super::{examples::CodeExample, tools::generate};
 
-const STARKWARE_JS_EXAMPLE: &str = "Writing...";
-const STARKWARE_CLI_EXAMPLE: &str = "Writing...";
-const STARKWARE_PYTHON_EXAMPLE: &str = "Writing...";
-const STARKWARE_GO_EXAMPLE: &str = "Writing...";
+const STARKWARE_JS_EXAMPLE: &str = r#"import { Provider } from "starknet";
+
+const provider = new Provider({ rpc: { nodeUrl: "${link}" } });
+let chainID = await provider.getChainId();
+
+console.log(chainID);"#;
+
+const STARKWARE_CLI_EXAMPLE: &str = r#"curl --url ${link} \
+-X POST \
+-H "Content-Type: application/json" \
+-d '{"jsonrpc":"2.0","method":"starknet_blockNumber","params":[],"id":1}'"#;
+
+const STARKWARE_PYTHON_EXAMPLE: &str = r#"import requests
+r = requests.post('${link}', json={
+                  'jsonrpc': '2.0', 'method': 'starknet_blockNumber', 'id': 1})
+print(r.json())
+"#;
+
+const STARKWARE_GO_EXAMPLE: &str = r#"package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+	data := map[string]any{
+		"jsonrpc": "2.0",
+		"method":  "starknet_blockNumber",
+		"id":      1,
+	}
+	bytesData, _ := json.Marshal(data)
+	req, _ := http.NewRequest("POST", "${link}", bytes.NewReader(bytesData))
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := client.Do(req)
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Print(string(body))
+}
+"#;
 
 pub fn get_starkware_examples(link: &str) -> CodeExample {
     CodeExample {
